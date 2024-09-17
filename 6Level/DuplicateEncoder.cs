a4a77f9290ed360d1d7e06049669960f7eacd794
Andrey <andrey.barsukov.bp@gmail.com>
2024-09-18 00:36:11 +0400

    + Task

---
+++ 6Level/DuplicateEncoder.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//https://www.codewars.com/kata/54b42f9314d9229fd6000d9c
+//Цель этого упражнения — преобразовать строку в новую строку, где каждый символ в новой строке — это "("если этот символ встречается только один раз в исходной строке или ")"
+//если этот символ встречается более одного раза в исходной строке. Игнорируйте регистр букв при определении того, является ли символ дубликатом.
+
+//    Примеры
+//"din"      =>  "((("
+//"recede"   =>  "()()()"
+//"Success"  =>  ")())())"
+//"(( @"     =>  "))((" 
+//Примечания
+//    Сообщения об утверждениях могут быть неясными относительно того, что они отображают в некоторых языках. Если вы читаете "...It Should encode XXX", то "XXX"это ожидаемый результат, а не ввод!
+
+namespace CodewarsTask._6Level
+{
+    public class DuplicateEncoder
+    {
+        public static string DuplicateEncode(string word)
+        {
+            word = word.ToUpper();
+            return string.Concat(word.Select(x => word.Count(e => e == x) > 1 ? ')' : '('));
+        }
+    }
+}
--- obj/Debug/net8.0/CodewarsTask.AssemblyInfo.cs
+++ obj/Debug/net8.0/CodewarsTask.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CodewarsTask")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60491c3a493aaf88dc7ed469eb526c2573c4d4c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("CodewarsTask")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CodewarsTask")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
