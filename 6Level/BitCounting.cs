b14fb008438928868959d8a149b43d23874c659e
Andrey <andrey.barsukov.bp@gmail.com>
2024-09-18 13:46:48 +0400

    refactoring

--- 6Level/BitCounting.cs
+++ 6Level/BitCounting.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+// https://www.codewars.com/kata/526571aae218b8ee490006f4
+//Напишите функцию, которая принимает целое число в качестве входных данных и возвращает количество битов,
+//которые равны единице в двоичном представлении этого числа. Вы можете гарантировать, что входные данные неотрицательны.
+
+//    Пример : Двоичное представление 1234равно , поэтому в этом случае 10011010010функция должна вернуть 5
 
 namespace CodewarsTask._6Level
 {
-    internal class Class1
+    internal class BitCounting
     {
+        public static int CountBits(int n)
+        {
+            return Convert.ToString(n, 2).ToCharArray().Where(e => !e.Equals('0')).Select(e => Convert.ToInt32(e.ToString())).Sum();
+        }
     }
 }
--- 6Level/CountingDuplicates.cs
+++ 6Level/CountingDuplicates.cs
@@ -4,9 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// https://www.codewars.com/kata/54bf1c2cd5b56cc47f0007a1
+//Подсчитайте количество дубликатов.
+//    Напишите функцию, которая будет возвращать количество различных нечувствительных к регистру буквенных
+// символов и числовых цифр, которые встречаются более одного раза во входной строке. Можно предположить,
+// что входная строка содержит только буквы алфавита (как заглавные, так и строчные) и числовые цифры.
+
+//    Пример
+//    «abcde» -> 0 # no characters repeats more than once
+//    «aabbcde» -> 2 # 'a' and 'b'
+//    «aabBcde» -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
+//    «неделимость» -> 1 # 'i' occurs six times
+//    «Неделимости» -> 2 # 'i' occurs seven times and 's' occurs twice
+//    «aA11» -> 2 # 'a' and '1'
+//    «ABBA» -> 2 # 'A' and 'B' each occur twice
+
 namespace CodewarsTask._6Level
 {
-    internal class Class1
+    internal class CountingDuplicates
     {
+        public static int DuplicateCount(string str)
+        {
+            return str.ToUpper().GroupBy(x => x).Where(x => x.Count() > 1).Count();
+        }
     }
 }
